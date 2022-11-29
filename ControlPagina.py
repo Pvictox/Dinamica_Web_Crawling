59b5f15502126c8ec860f9488258a00375711d5d
Pedro <paf.victor@gmail.com>
2022-11-29 08:38:08 -0300

    Mudança na relevancia de cada pagina

--- ControlPagina.py
+++ ControlPagina.py
@@ -9,9 +9,36 @@ def aplicarRelevancia(termo, dicionarioPag):
             relevanciaTermo2 = 0
             relevanciaTermo1 = calcularRelevancia(termo=split_Termo[0], pagina=value)
             relevanciaTermo2 = calcularRelevancia(termo=split_Termo[1], pagina=value)
-            print("REL termo 1: ", relevanciaTermo1)
-            print("REL termo 2: ", relevanciaTermo2)
+            
+            #Ponderada
+            pesoTermo1 = 50
+            pesoTermo2 = 50
+            if (relevanciaTermo1 != 0 and relevanciaTermo2 != 0):
+                pesoTermo1 = pesoTermo1- (0.1*relevanciaTermo1)
+                relevanciaTermo1 = (relevanciaTermo1*pesoTermo1)//100
+                pesoTermo2 = pesoTermo2- (0.1*relevanciaTermo2)
+                relevanciaTermo2 = (relevanciaTermo2*pesoTermo2)//100
+                relevanciaPonderada = relevanciaTermo2+relevanciaTermo1
+            else:
+                relevanciaPonderada = 0
+            
 
+            #Busca pela junção completa
+            relevanciaTermoJuncao = calcularRelevanciaTermoJunto(termo=termo, pagina=value)
+        
+            value.setRelevancia((relevanciaPonderada+relevanciaTermoJuncao)/2)
+
+
+                 
+
+
+def calcularRelevanciaTermoJunto(termo, pagina):
+    relevanciaTermo = 0        
+    if (pagina.getTitulo().lower().__contains__(termo)): #Verifica titulo
+        relevanciaTermo+=40
+    if (pagina.getTexto().lower().__contains__(termo)):
+        relevanciaTermo += pagina.getTexto().lower().count(termo)*20
+    return relevanciaTermo
 
 def calcularRelevancia(termo, pagina):
     relevanciaTermo = 0        
