cfbad54cb866142a9154aefa23fe55a8fefacaf6
Pedro <paf.victor@gmail.com>
2022-12-11 11:39:29 -0300

    Adicionando pesquisa de duas palavras

--- ControlPagina.py
+++ ControlPagina.py
@@ -22,6 +22,40 @@ def searchTermo(termo, dicionarioPag):
                 result[value] = relevanciaTermoTexto+relevanciaTermoTitulo
             cache[termo_splitado[0]] = result
             return result
+    elif(len(termo_splitado) == 2):
+        termo = termo.lower()
+        if (termo in cache):
+            print("Palavra previamente pesquisada | Retornando Cache")
+            return cache[termo]
+        else:
+            for key, value in dicionarioPag.items():
+                dicionarioTitulo = value.getPalavrasTitulo()
+                termo1 = termo_splitado[0]
+                termo2 = termo_splitado[1]
+                relevanciaTermo1 = 0
+                relevanciaTermo2 = 0
+                if (termo1 in dicionarioTitulo):
+                    relevanciaTermo1 = dicionarioTitulo[termo1] * 30
+                if (termo2 in dicionarioTitulo):
+                    relevanciaTermo2 = dicionarioTitulo[termo2] * 30
+                if (termo1 in value.getPalavrasTexto()):
+                    relevanciaTermo1 = value.getPalavrasTexto()[termo1] * 10
+                if (termo2 in value.getPalavrasTexto()):
+                    relevanciaTermo2 = value.getPalavrasTexto()[termo2] * 10
+                
+                pesoTermo1 = 50
+                pesoTermo2 = 50
+                relevanciaPonderada = 0
+                if (relevanciaTermo1 != 0 and relevanciaTermo2 != 0):
+                    pesoTermo1 = pesoTermo1- (0.1*relevanciaTermo1)
+                    relevanciaTermo1 = (relevanciaTermo1*pesoTermo1)//100
+                    pesoTermo2 = pesoTermo2- (0.1*relevanciaTermo2)
+                    relevanciaTermo2 = (relevanciaTermo2*pesoTermo2)//100
+                    relevanciaPonderada = relevanciaTermo2+relevanciaTermo1
+                result[value] = relevanciaPonderada
+            cache[termo] = result
+            return result
+
         
 
                  
